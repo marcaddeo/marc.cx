97b145fa5c1177e083ebb68055dc5646728e3a10
Marc Addeo <marcaddeo@gmail.com>
2015-05-26 22:58:52 -0400

    Move static assets around and configure brunch

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,13 +1,5 @@
-import {Socket} from "phoenix"
-
-// let socket = new Socket("/ws")
-// socket.connect()
-// let chan = socket.chan("topic:subtopic", {})
-// chan.join().receive("ok", chan => {
-//   console.log("Success!")
-// })
+hljs.initHighlightingOnLoad();
 
 let App = {
 }
-
 export default App
