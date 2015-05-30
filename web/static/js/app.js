df33d044fb14d8138cc3f9eb3fdfaefd1d4f2b24
Marc Addeo <marcaddeo@gmail.com>
2015-05-30 11:25:50 -0400

    Make all external links have a target=_blank, using javascript

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,5 +1,14 @@
 hljs.initHighlightingOnLoad();
 
+jQuery.expr[':'].external = function(obj){
+    return !obj.href.match(/^mailto\:/)
+           && (obj.hostname != location.hostname)
+           && !obj.href.match(/^javascript\:/)
+           && !obj.href.match(/^$/)
+};
+
+jQuery('a:external').attr('target', '_blank');
+
 let App = {
 }
 export default App
