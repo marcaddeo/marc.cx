2d233a095cad9099c5f8e22fd22234811956cfbc
Marc Addeo <marcaddeo@gmail.com>
2015-06-01 21:53:11 -0400

    Add a format_date function to the PageView for formatting published dates from Articles

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,25 @@
 defmodule MarcCx.PageView do
   use MarcCx.Web, :view
+  use Timex
+  alias MarcCx.Article
+
+  def format_date(%Article{:timestamp => timestamp}) do
+    {:ok, date} = DateFormat.parse("#{timestamp}", "{s-epoch}")
+
+    [month, day, year] =
+      date
+      |> DateFormat.format!("%B %d %Y", :strftime)
+      |> String.split(" ")
+
+    day = day |> String.lstrip(?0)
+
+    day = case String.last(day) do
+      "1" -> "#{day}st"
+      "2" -> "#{day}nd"
+      "3" -> "#{day}rd"
+      _   -> "#{day}th"
+    end
+
+    "#{month} #{day} #{year}"
+  end
 end
