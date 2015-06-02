70405c52014efc4d9d9aa4995b550ecbde9551ae
Marc Addeo <marcaddeo@gmail.com>
2015-06-01 21:54:18 -0400

    Update the ArticleController to use the new Article model

--- web/controllers/article_controller.ex
+++ web/controllers/article_controller.ex
@@ -1,38 +1,22 @@
 defmodule MarcCx.ArticleController do
   use MarcCx.Web, :controller
-  alias MarcCx.Markdown
+  alias MarcCx.Article
 
   plug :action
 
   def index(conn, %{"slug" => slug}) do
-    case read_article(slug) do
-      {:ok, content} ->
-        {metadata, content} = Markdown.parse(content)
-
+    case Article.get_article(slug) do
+      {:ok, article} ->
         assigns = %{
-          content: content,
-          title: metadata.title,
-          published: metadata.published
+          content: article.html,
+          title: article.title
         }
 
         render conn, "index.html", assigns
-      {:error, :not_found} ->
+      {:error, :notfound} ->
         conn
         |> put_status(:not_found)
         |> render(MarcCx.ErrorView, "404.html")
-    end
-  end
-
-  defp read_article(slug) do
-    path = Application.app_dir(:marc_cx, "priv/articles/#{slug}.md")
-
-    if File.exists?(path) do
-      content = File.read!(path)
-      |> String.split(~r{\r\n?|\n})
-
-      {:ok, content}
-    else
-      {:error, :not_found}
     end
   end
 end
