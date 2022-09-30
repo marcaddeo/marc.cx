91276f861e5eff3ca4fa0e2ef2aa6f730d0175a7
Marc Addeo <marcaddeo@gmail.com>
2022-09-30 16:11:19 -0400

    Scaffold a rust/svelte app

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+#[macro_use] extern crate rocket;
+use rocket::fs::{FileServer, relative};
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", FileServer::from(relative!("static")))
+}
