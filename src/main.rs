35f8b9388e6d7ad0a4c7ae9613f03bf3fedb8d45
Marc Addeo <marcaddeo@gmail.com>
2022-10-01 10:52:00 -0400

    Convert site to svelte-ish

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 #[macro_use] extern crate rocket;
 use rocket::fs::{FileServer, relative};
+use rocket::http::ContentType;
+
+#[get("/global.css")]
+fn global_css() -> (ContentType, String) {
+    let sass = grass::from_path(
+        relative!("static/global.css"),
+        &grass::Options::default()
+    ).unwrap();
+
+    (ContentType::CSS, sass)
+}
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .mount("/", routes![global_css])
         .mount("/", FileServer::from(relative!("static")))
 }
