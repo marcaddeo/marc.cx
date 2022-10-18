f0be4f2949e5682c74e2aa08e7e58042d9c3a98a
Marc Addeo <marcaddeo@gmail.com>
2022-10-17 21:38:39 -0400

    Set highlight style

--- client/components/CodeBlock.wc.svelte
+++ client/components/CodeBlock.wc.svelte
@@ -4,17 +4,19 @@
   import * as languages from "svelte-highlight/languages";
   import * as styles from "svelte-highlight/styles";
 
-  export let style = "github";
+  export let style: string = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/base16/paraiso.min.css";
   export let code: string;
   export let language: string;
 </script>
 
 <link rel="stylesheet" href="/static/build/bundle.css">
 
-<div>
-  {#if styles[style]}
+{#if styles[style]}
+  <div>
     {@html styles[style]}
-  {/if}
-</div>
+  </div>
+{:else}
+  <link rel="stylesheet" href="{style}">
+{/if}
 
 <Highlight language={languages[language]} {code} />
